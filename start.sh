a72088677f4540f9d2546064364c8d1d4df8a69e
bluejamjw-creator <bluejam.jw@gmail.com>
2026-04-20 16:10:30 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+set -eu
 
 echo "Starting Sniper + Portfolio..."
 
 node sniper.js &
-python portfolio_bot.py
+SNIPER_PID=$!
+
+python portfolio_bot.py &
+PORTFOLIO_PID=$!
+
+trap 'echo "Stopping..."; kill $SNIPER_PID $PORTFOLIO_PID 2>/dev/null || true' INT TERM
+
+wait $SNIPER_PID $PORTFOLIO_PID
